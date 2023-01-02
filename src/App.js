fbe32118f044a919c91aba761c5897fa6e213f93
Olexander Mazanyk <grejnext@gmail.com>
2023-01-02 20:27:11 +0200

    create skeleton

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from "react";
 import Header from "./component/Header/Header";
 import Categories from "./component/Categories/Categories";
 import Sort from "./component/Sort/Sort";
-import PIzzaList from "./component/Pizza/PIzzaList";
+import PizzaList from "./component/Pizza/PizzaList";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch("http://localhost:3000/items")
+    fetch("http://localhost:3001/items")
       .then((res) => res.json())
-      .then((res) => setItems(res));
+      .then((res) => {
+        setItems(res);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -23,7 +27,7 @@ function App() {
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
-          <PIzzaList pizza={items} />
+          <PizzaList isLoading={isLoading} pizza={items} />
         </div>
       </div>
     </div>
--- src/component/Pizza/PIzzaList.jsx
+++ src/component/Pizza/PIzzaList.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import PizzaItem from "./PizzaItem";
+import Skeleton from "./Skeleton";
 
-function PIzzaList({ pizza }) {
+function PizzaList({ pizza, isLoading }) {
   return (
     <div className="content__items">
-      {pizza.map((item, i) => (
-        <PizzaItem key={i} {...item} />
-      ))}
+      {isLoading
+        ? [...new Array(8)].map((_, index) => (
+            <Skeleton className="pizza-block" key={index} />
+          ))
+        : pizza.map((item, i) => <PizzaItem key={i} {...item} />)}
     </div>
   );
 }
 
-export default PIzzaList;
+export default PizzaList;
